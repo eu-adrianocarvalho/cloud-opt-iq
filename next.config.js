7d3b60770502bb1128dbde2333159f557bfbd800
driicarvalho7 <adriano.carvalho2003@hotmail.com>
2025-07-07 15:29:24 -0300

    a

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/cloudoptiq" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/cloudoptiq" : "",
 };
 
 module.exports = nextConfig;
